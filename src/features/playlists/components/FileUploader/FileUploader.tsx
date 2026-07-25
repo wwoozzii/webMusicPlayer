350b20ff108fb4a4d4eca3f4cd03564968df79d6
wwoozzii <gmar4630@gmail.com>
2026-07-25 21:39:39 +0300

    docs(FileUploader): add todo comment for parsing const

--- src/features/playlists/components/FileUploader/FileUploader.tsx
+++ src/features/playlists/components/FileUploader/FileUploader.tsx
@@ -9,6 +9,8 @@ export const FileUploader = () => {
     const file = e.target.files?.[0];
     if (file) {
       const trackUrl = URL.createObjectURL(file);
+      //сменить способ получения данных
+      //addTrack({ url: trackUrl, name: file.name.replace('.mp3" , "")})
       addTrack({ url: trackUrl, name: file.name });
       setLoadFile(!loadFile);
     }
