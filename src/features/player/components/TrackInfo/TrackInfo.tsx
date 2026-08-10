3a7b40f16939f9e47589e0619fb33eba94e9cf6c
wwoozzii <gmar4630@gmail.com>
2026-08-10 17:26:57 +0300

    feat(TrackInfo): add author to trackinfo

--- src/features/player/components/TrackInfo/TrackInfo.tsx
+++ src/features/player/components/TrackInfo/TrackInfo.tsx
@@ -2,11 +2,12 @@ import { usePlayerStore } from "../../playerStore";
 
 export const TrackInfo = () => {
   const currentTrack = usePlayerStore((state) => state.currentTrack);
+
   if (!currentTrack) return null;
 
   return (
     <div>
-      <strong>Играет:</strong> {currentTrack.name}
+      <strong>{`Играет: ${currentTrack.name} — ${currentTrack.author}`}</strong>
     </div>
   );
 };
