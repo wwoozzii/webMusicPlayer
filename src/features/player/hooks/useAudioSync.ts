d7bc2195d28c678fcb14a3504964bd28ff8f414a
wwoozzii <gmar4630@gmail.com>
2026-08-16 17:58:58 +0300

    feat(useAudioSync): add seekTime handling

--- src/features/player/hooks/useAudioSync.ts
+++ src/features/player/hooks/useAudioSync.ts
@@ -6,14 +6,24 @@ export const useAudioSync = (
 ) => {
   const isPlaying = usePlayerStore((state) => state.isPlaying);
   const currentTrack = usePlayerStore((state) => state.currentTrack);
+  const seekTime = usePlayerStore((state) => state.seekTime);
+  const setStore = usePlayerStore.setState;
 
   useEffect(() => {
-    if (!audioRef.current) return;
+    if (!audioRef.current || !currentTrack) return;
 
     if (isPlaying) {
       audioRef.current.play().catch((err) => console.log("ошибка", err));
     } else {
       audioRef.current.pause();
     }
-  }, [isPlaying, currentTrack, audioRef]);
+  }, [isPlaying, audioRef, currentTrack]);
+
+  useEffect(() => {
+    if (audioRef.current && seekTime !== null) {
+      audioRef.current.currentTime = seekTime;
+
+      setStore({ seekTime: null });
+    }
+  }, [seekTime, audioRef, setStore]);
 };
