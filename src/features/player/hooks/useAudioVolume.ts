d19114e31bda705dd790c22866ef833dfd0d3085
wwoozzii <gmar4630@gmail.com>
2026-08-16 18:02:46 +0300

    refactor(useAudioVolume): extract constants from state and add dependencies

--- src/features/player/hooks/useAudioVolume.ts
+++ src/features/player/hooks/useAudioVolume.ts
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
+import { usePlayerStore } from "../playerStore";
 
 export const useAudioVolume = (
   audioRef: React.RefObject<HTMLAudioElement | null>,
-  currentVolume: number,
 ) => {
+  const currentVolume = usePlayerStore((state) => state.currentVolume);
+  const currentTrack = usePlayerStore((state) => state.currentTrack);
+
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.volume = currentVolume;
     }
-  }, [currentVolume, audioRef]);
+  }, [currentVolume, currentTrack, audioRef]);
 };
